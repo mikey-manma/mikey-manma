53b85894fb57de3b26955879a411b91dbbbdf8e9
mikey-manma <53634961+mikey-manma@users.noreply.github.com>
2019-08-14 13:02:37 +0900

    Add files via upload

---
+++ 1-2a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a,b = input().split()
+if(int(a)>int(b)):
+    print("a>b")
+elif(int(a)<int(b)):
+    print("a<b")
+elif(int(a)==int(b)):
+    print("a==b")
+
+
+# In[ ]:
+
+
+
+

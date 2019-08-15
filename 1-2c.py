618be612e2e642c8d6f4ac9f6eb476f23505288f
mikey-manma <53634961+mikey-manma@users.noreply.github.com>
2019-08-15 22:32:08 +0900

    Add files via upload

---
+++ 1-2c.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+a,b,c = input().split()
+d = [int(a),int(b),int(c)]
+d.sort()
+print(str(d[0])+" "+str(d[1])+" "+str(d[2]))
+
+
+# In[ ]:
+
+
+
+

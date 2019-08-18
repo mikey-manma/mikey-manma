861ad8cb73f9dc4e6a2485a1a10853b2bd886c9d
mikey-manma <53634961+mikey-manma@users.noreply.github.com>
2019-08-18 14:25:43 +0900

    Add files via upload

---
+++ 1-3b.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+m=0
+n=1
+l = list(iter(input,"0"))
+while(l[m]!=0):
+    print("Case "+ str(n) +": "+str(l[m]))
+    n=n+1
+    m=m+1
+
+
+# In[ ]:
+
+
+
+

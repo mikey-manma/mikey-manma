36408b0ba4faa7f69eda92fe24e962a673ebbeed
mikey-manma <53634961+mikey-manma@users.noreply.github.com>
2019-09-10 17:30:15 +0900

    Add files via upload

---
+++ 1-4a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a,b=input().split()
+d=int(a)//int(b)
+r=int(a)%int(b)
+f=float(a)/float(b)
+print(str(d)+" "+str(r)+" "+str(f))
+
+
+# In[ ]:
+
+
+
+

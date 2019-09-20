377b799e4bdbe8641efe9b86a3a6850d1ab04531
mikey-manma <53634961+mikey-manma@users.noreply.github.com>
2019-09-20 14:53:32 +0900

    Add files via upload

---
+++ 1-5d.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+a=input()
+b=0
+b=1
+for i in range(int(a)):
+    if(b%3==0):
+        print(str(b)+" ", end="")
+    b=b+1
+
+
+# In[ ]:
+
+
+
+

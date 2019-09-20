36ca83ffbce022af671e75d17735e74cc5939923
mikey-manma <53634961+mikey-manma@users.noreply.github.com>
2019-09-20 12:31:29 +0900

    Add files via upload

---
+++ 1-5a.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[24]:
+
+
+a,b = input().split()
+c=[]
+d=0
+for i in range(int(b)):
+    c.append("#")
+for i in range(int(a)):
+    print(c)
+
+
+# In[ ]:
+
+
+
+

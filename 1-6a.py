8c21a1a1a356683ac4cb00e70f9c56eb096c0365
mikey-manma <53634961+mikey-manma@users.noreply.github.com>
2019-09-20 15:31:12 +0900

    Add files via upload

---
+++ 1-6a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=input()
+b=input().split()
+c=int(a)-1
+for i in range(int(a)):
+    print(str(b[c])+" ", end="")
+    c=c-1
+
+
+# In[ ]:
+
+
+
+

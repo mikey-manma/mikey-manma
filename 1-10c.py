0919f86a37d4f00468cda2a3ed71650020dbb9c9
mikey-manma <53634961+mikey-manma@users.noreply.github.com>
2019-09-24 13:08:29 +0900

    Add files via upload

---
+++ 1-10c.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+while True:
+    n=int(input())
+    if n == 0:
+        break
+    s=list(map(int,input().split()))
+    m=sum(s)/n
+    v=0
+    for si in s:
+        v+=(si-m)**2
+    ans=(v/n)**0.5
+    print("{:.5f}".format(ans))
+
+
+# In[ ]:
+
+
+
+
